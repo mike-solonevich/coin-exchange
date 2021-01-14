8a114afedffba2f9a4257716e61da7d7e04d328e
michael <mike.solonevich@gmail.com>
2021-01-14 18:51:28 +0300

    Styled componends update

---
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const Section = styled.section`
+  margin: 40px auto 0;
+  color: #fff;
+  font-size: 1.6rem;
+  text-decoration: underline;
+`;
+
+export default class AccountBalance extends Component {
+  render() {
+    return (
+      <Section>
+        Your USD balance: ${this.props.amount}
+      </Section>
+    );
+  }
+}
+
+AccountBalance.propTypes = {
+  amount: PropTypes.number.isRequired
+}
